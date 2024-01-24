4b4b563c8d49f878c67933796c0d9e2389036cec
pavva91 <valerio.mattioli@proton.me>
2024-01-24 14:35:13 +0100

    fix script folder

--- scripts/setVars.sh
+++ scripts/setVars.sh
@@ -4,6 +4,6 @@ DIRPATH=$(/usr/bin/dirname $(pwd))
 
 export PATH=$PATH:$DIRPATH/minio-binaries
 
-export KES_CLIENT_KEY=$DIRPATH/dummy-identity/root.key
-export KES_CLIENT_CERT=$DIRPATH/dummy-identity/root.cert
+export KES_CLIENT_KEY=$DIRPATH/kms-identity/root.key
+export KES_CLIENT_CERT=$DIRPATH/kms-identity/root.cert
 export KES_SERVER=https://play.min.io:7373
